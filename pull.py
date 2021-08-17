de0999e05962b139bc76f0974433dd39bf3c414f
DESKTOP-O5RNJJH\admin <suge@yadingdata.com>
2021-08-17 17:36:50 +0800

    pull.py

---
+++ pull.py
@@ -0,0 +1,16 @@
+import subprocess
+from datetime import datetime
+while True:
+    # Run every hour
+    now = datetime.now()
+    if now.minute != 0:
+        # Commit every 3h
+        if now.hour % 3 != 0:
+            try:
+                subprocess.run('git pull')
+            except:
+                pass
+        time.sleep(60 * 50)
+    else:
+        time.sleep(1)
+  
